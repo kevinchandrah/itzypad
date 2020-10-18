5c972f9e50dad23023158fa3aa2e8d2c971fbf94
Neoeu <70676179+Neoeu@users.noreply.github.com>
2020-10-18 21:12:32 +0700

    Update buku_js.jsx

--- js/buku_js.jsx
+++ js/buku_js.jsx
@@ -10,8 +10,8 @@ data_buku.push("Alice's Adventures in Wonderland",
                "Lewis Carroll",
                "Through the Looking-Glass",
                "1987",
-               "http://localhost/asset/book_and_cover/image/A_second_chance.jpg",
-               "http://localhost/asset/book_and_cover/Alice's_Adventures_in_Wonderland.html"
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/image/A_second_chance.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/Alice's_Adventures_in_Wonderland.html"
                );
 data_buku.push("Ironi, Islandia dalam Kekelaman",
                "amsiong was desperately trying to go out from his position that trap in bottomless pit dungeon with himself and simple magic stick.",
@@ -20,8 +20,8 @@ data_buku.push("Ironi, Islandia dalam Kekelaman",
                "Michelle",
                "",
                "2018",
-               "http://localhost/asset/image/itzypad.jpg",
-               "http://localhost/asset/book_and_cover/Ironi_Islandia_dalam_Kekelaman.html"
+               "https://kevinchandrah.github.io/itzypad/asset/image/itzypad.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/Ironi_Islandia_dalam_Kekelaman.html"
                );
 data_buku.push("Romeo and Juliet",
                "An age-old vendetta between two powerful families erupts into bloodshed. A group of masked Montagues risk further conflict by gatecrashing a Capulet party. A young lovesick Romeo Montague falls instantly in love with Juliet Capulet, who is due to marry her father’s choice, the County Paris. With the help of Juliet’s nurse, the women arrange for the couple to marry the next day, but Romeo’s attempt to halt a street fight leads to the death of Juliet’s own cousin, Tybalt, for which Romeo is banished. In a desperate attempt to be reunited with Romeo, Juliet follows the Friar’s plot and fakes her own death. The message fails to reach Romeo, and believing Juliet dead, he takes his life in her tomb. Juliet wakes to find Romeo’s corpse beside her and kills herself. The grieving family agree to end their feud.",
@@ -30,8 +30,8 @@ data_buku.push("Romeo and Juliet",
                "N/A",
                "William Shakespeare",
                "1597",
-               "http://localhost/asset/image/itzypad.jpg",
-               "http://localhost/asset/book_and_cover/Romeo_and_Juliet.html"
+               "https://kevinchandrah.github.io/itzypad/asset/image/itzypad.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/Romeo_and_Juliet.html"
                );
 data_buku.push("The Art of War",
                "The Art of War is a Chinese military treatise that was written during the 6th century BC by Sun Tzu. Composed of 13 chapters, each of which is devoted to one aspect of warfare, it has long been praised as the definitive work on military strategies and tactics of its time. The Art of War is one of the oldest books on military strategy in the world. It is the first and one of the most successful works on strategy and has had a huge influence on Eastern and Western military thinking, business tactics, and beyond. Sun Tzu was the first to recognize the importance of positioning in strategy and that position is affected both by objective conditions in the physical environment and the subjective opinions of competitive actors in that environment. He taught that strategy was not planning in the sense of working through a to-do list, but rather that it requires quick and appropriate responses to changing conditions. Planning works in a controlled environment, but in a competitive environment.",
@@ -40,8 +40,8 @@ data_buku.push("The Art of War",
                "Sun Tzu",
                "",
                "5th century BC",
-               "http://localhost/asset/image/itzypad.jpg",
-               "http://localhost/asset/book_and_cover/The_Art_of_War.html"
+               "https://kevinchandrah.github.io/itzypad/asset/image/itzypad.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/The_Art_of_War.html"
                );
 data_buku.push("The Adventures of Sherlock Holmes",
                "The Adventures of Sherlock Holmes is the series of short stories that made the fortunes of the Strand magazine, in which they were first published, and won immense popularity for Sherlock Holmes and Dr Watson. The detective is at the height of his powers and the volume is full of famous cases, including 'The Red-Headed League', 'The Blue Carbuncle', and 'The Speckled Band'. Although Holmes gained a reputation for infallibility, Conan Doyle showed his own realism and feminism by having the great detective defeated by Irene Adler - the woman - in the very first story, 'A Scandal in Bohemia'.",
@@ -50,8 +50,8 @@ data_buku.push("The Adventures of Sherlock Holmes",
                "Arthur Conan Doyle",
                "The Memoirs of Sherlock Holmes, The Sign of the Four",
                "1892",
-               "http://localhost/asset/book_and_cover/The_Adventures_of_Sherlock_Holmes.jpg",
-               "http://localhost/asset/book_and_cover/The_Adventures_of_Sherlock_Holmes.html"
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/The_Adventures_of_Sherlock_Holmes.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/The_Adventures_of_Sherlock_Holmes.html"
                );
 data_buku.push("The Murder on the Links",
                "The Murder on the Links is a work of detective fiction by Agatha Christie and first published in the UK by The Bodley Head in May 1923 and in the US by Dodd, Mead and Company in the same year.",
@@ -60,8 +60,8 @@ data_buku.push("The Murder on the Links",
                "Agatha Christie",
                "The Secret Adversary, Poirot Investigates",
                "1923",
-               "http://localhost/asset/book_and_cover/The_Murder_on_the_Links.jpg",
-               "http://localhost/asset/book_and_cover/The_Murder_on_the_Links.html"
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/The_Murder_on_the_Links.jpg",
+               "https://kevinchandrah.github.io/itzypad/asset/book_and_cover/The_Murder_on_the_Links.html"
                );
 
 function ambil_buku(){
