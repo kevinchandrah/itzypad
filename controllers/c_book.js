b6ce2d758ed8ec50f1db70f9b90dea2175344da8
kevinchandrah <70614349+kevinchandrah@users.noreply.github.com>
2020-12-14 23:19:40 +0700

    added control_book.js

--- controllers/c_book.js
+++ controllers/c_book.js
@@ -1,1 +1,31 @@
+const connect_book = require('../models/model_book');
 
+async function find(){
+    return await connect_book.find({});
+}
+async function findSearch(data){
+    //const query = {$text: {$search: data}};
+    /*console.log(connect_book.find({ $text: { $search: data } }));
+    try{
+        return await connect_book.find({ $text: { $search: data } });
+
+    } catch {
+        console.log("fail (tidak ada salah)");
+    }*/
+    return await connect_book.find({});
+    
+    /*const query ={title: data};
+    return await connect_book.find(query);*/
+
+}
+async function findDescription(data){
+    const query = {title: data};
+    return await connect_book.find(query);
+}
+
+
+module.exports = {
+    find,
+    findSearch,
+    findDescription
+}
